0c44121593fce32c993092f0a82c4c791b344b4b
Ales Kozumplik <akozumpl@redhat.com>
2011-10-25 15:15:08 +0200

    replicate what xfce is doing (without the keyboard grab)

--- lightbox.c
+++ lightbox.c
@@ -23,7 +23,11 @@ int main (int argc, char **argv)
     fadeout = show_fadeout();
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show(window);
+    gtk_widget_realize(window);
+    gdk_window_set_override_redirect (window->window, TRUE);
+    gdk_window_raise(window->window);
+    gtk_widget_show_now(window);
+
 
     gtk_main();
     hide_fadeout(fadeout);
